900e2028666cdd901be7d96a5644587ecbc923e5
Haegnim <haegnim@gmail.com>
2021-07-06 15:03:52 +0900

    0706 아코디언 탭 & 스크롤 슬라이드 창

--- js/jq.main.js
+++ js/jq.main.js
@@ -21,6 +21,57 @@ $(function () {
     $(".subnav_box").hide();
     $(".subnav_box").eq(i).show();
   });
+
+  //mobile menu animation effect
+  $(".menu_icon").hover(function () {
+    $(this).toggleClass("active");
+  });
+  //mobile menu click effect
+  $(".menu_icon, .overlay").click(function () {
+    $(".mobile_nav").animate({ left: 0 }, 300);
+    $(".section").animate({ left: "60%" }, 300);
+    $(".overlay").css(
+      {
+        opacity: 0.7,
+        left: "60%",
+        width: "100%",
+      },
+      300
+    );
+  });
+
+  $(".close_btn, .overlay").click(function () {
+    $(".mobile_nav").animate({ left: "-60%" }, 300);
+    $(".section").animate({ left: 0 }, 300);
+    $(".overlay").animate({ opacity: 0 }, 300);
+    $(".overlay").css(
+      {
+        opacity: 0,
+        left: 0,
+        width: 0,
+      },
+      300
+    );
+  });
+
+  //mobile menu animation effect
+
+  // mobile menu accordion effect
+  $(".mobile_tit").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).removeClass("rotate");
+      $(this).siblings(".mobile_sub_nav").slideUp(200);
+    } else {
+      $(".mobile_tit").removeClass("active");
+      $(".mobile_sub_nav").slideUp(200);
+      $(".mobile_tit").removeClass("rotate");
+
+      $(this).addClass("active");
+      $(this).siblings(".mobile_sub_nav").slideDown(200);
+      $(this).addClass("rotate");
+    }
+  });
 });
 
 // $(document).ready(function(){});
