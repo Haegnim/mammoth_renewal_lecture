8064c7b6572e2900e26deb069ee1e5ad04fdbdd9
Haegnim <haegnim@gmail.com>
2021-07-07 18:28:44 +0900

    0707 반응형 디자인

--- js/fullpage_code.js
+++ js/fullpage_code.js
@@ -1,5 +1,15 @@
 $(function () {
   $("#fullpage").fullpage({
+    navigation: true,
+    fixedElements: "#header",
+    verticalCentered: false,
+    responsiveWidth: 700,
     anchors: ["1st", "2nd", "3rd"],
+    afterLoad: function (anchorLink, index) {
+      if (anchorLink == "2nd") {
+        $(".page_txt_box .main_txt").addClass("txt");
+        $(".page_txt_box .page_logo").addClass("txt_up");
+      }
+    },
   });
 });
